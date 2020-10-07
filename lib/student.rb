40afc17a020740b1b6361ca701f69a795e276293
Ariel Ibarra <arielibarra39@gmail.com>
2020-10-07 23:38:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,21 @@
 class Student
 
+  attr_accessor :name, :grade
+  attr_reader :id 
+  
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id 
+  end 
+
+  
+  
+  
+end
 
 
 
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
-end
+  #  with DB[:conn]
