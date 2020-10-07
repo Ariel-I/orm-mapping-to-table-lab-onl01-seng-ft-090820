c97ca9d6f398d56602d23332c6b71ce6497717e5
Ariel Ibarra <arielibarra39@gmail.com>
2020-10-07 23:39:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,10 +12,10 @@ class Student
   
   def self.create_table
     sql =  <<-SQL 
-      CREATE TABLE IF NOT EXISTS songs (
+      CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY, 
         name TEXT, 
-        album TEXT
+        age TEXT
         )
         SQL
     DB[:conn].execute(sql) 
